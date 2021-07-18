1f6b672c9cf8907a7ec91a736490bca5053d72dd
Michiel van Oosterhout <git@michielvoo.net>
2021-07-18 23:22:15 +0200

    Import all AWS modules in profile

--- aws/test.ps1
+++ aws/test.ps1
@@ -1,4 +1,3 @@
-Import-Module AWS.Tools.CloudFormation
 Set-AWSCredential -AccessKey $Env:AWS_ACCESS_KEY -SecretKey $Env:AWS_SECRET_KEY -StoreAs default
 Set-DefaultAWSRegion -Region $Env:AWS_REGION
 Get-CFNStack
