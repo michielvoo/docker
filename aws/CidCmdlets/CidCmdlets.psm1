f11fe014568628059af828bcea912227a669a1fb
Michiel van Oosterhout <git@michielvoo.net>
2021-08-01 19:11:55 +0200

    Export CI/CD context

--- aws/CidCmdlets/CidCmdlets.psm1
+++ aws/CidCmdlets/CidCmdlets.psm1
@@ -1,5 +1,5 @@
 . $PSScriptRoot/CidContext.ps1
 . $PSScriptRoot/Logging.ps1
 
-Set-Variable -Name "CidContext" -Value "test" -Option "Constant"
+Set-Variable -Name "CidContext" -Value (Get-CidContext) -Option "Constant"
 Export-ModuleMember -Variable "CidContext"
