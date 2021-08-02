3fb1fe53c3523ac1272a53b72166b855eb9d0b3c
Michiel van Oosterhout <git@michielvoo.net>
2021-08-02 13:07:46 +0200

    Write the CI/CD context when the module is imported

--- aws/CidCmdlets/CidCmdlets.psm1
+++ aws/CidCmdlets/CidCmdlets.psm1
@@ -1,5 +1,23 @@
 . $PSScriptRoot/CidContext.ps1
 . $PSScriptRoot/Logging.ps1
 
+If ((Test-Path -Path "Env:AWS_ACCESS_KEY_ID") -and (Test-Path -Path "Env:AWS_SECRET_ACCESS_KEY"))
+{
+    Write-Host "Saving AWS credentials to persistent store"
+    Set-AWSCredential -StoreAs "default" -AccessKey $Env:ACCESS_KEY -SecretKey $Env:AWS_SECRET_KEY
+}
+
+If (Test-Path -Path "Env:AWS_DEFAULT_REGION")
+{
+    Write-Host "Setting default AWS region into the shell environment"
+    Set-DefaultAWSRegion -Region $Env:AWS_DEFAULT_REGION
+}
+
 Set-Variable -Name "CidContext" -Value (Get-CidContext) -Option "Constant"
 Export-ModuleMember -Variable "CidContext"
+
+$CidContext |
+    Format-Table -AutoSize -HideTableHeaders |
+    Out-String -Stream |
+    Where-Object { $_ -ne "" } |
+    ForEach-Object { Write-Host $_ }
