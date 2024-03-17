5f70e84d791e4c82b57948fddc278bdce77a8e17
Michiel van Oosterhout <git@michielvoo.net>
2024-03-17 07:59:22 +0300

    Use GitHub environment variable if available during tests

--- ci/powershell/Dockerfile.Tests.ps1
+++ ci/powershell/Dockerfile.Tests.ps1
@@ -4,8 +4,14 @@ BeforeAll{
     $ancestor = Split-Path "$PSScriptRoot" -Parent
     while ($true) {
         if (Get-ChildItem $ancestor "License.txt") {
-            $ancestor = Split-Path $ancestor -Parent
-            $name = "$(Split-Path $ancestor -Leaf)/$name"
+            if ($Env:GITHUB_REPOSITORY) {
+                $name = "$("$Env:GITHUB_REPOSITORY".Split("/")[0])/$name"
+            }
+            else {
+                $ancestor = Split-Path $ancestor -Parent
+                $name = "$(Split-Path $ancestor -Leaf)/$name"
+            }
+
             break
         }
 
@@ -14,6 +20,7 @@ BeforeAll{
     }
 
     $tag = "$name`:test"
+    Write-Warning $tag
 
     docker build "$PSScriptRoot" --tag "$tag" 2>&1 > $null
     $containerId = docker run --detach --interactive "$tag"
