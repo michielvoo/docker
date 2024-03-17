365773de926821281cca4f38729c77de8af113de
Michiel van Oosterhout <michielvoo@users.noreply.github.com>
2024-03-17 08:00:33 +0300

    Use fully qualified image names (#2)

--- ci/powershell/Dockerfile.Tests.ps1
+++ ci/powershell/Dockerfile.Tests.ps1
@@ -1,19 +1,41 @@
-BeforeAll {
-    $tag = [System.Guid]::NewGuid().ToString("N")
-    docker build "$PSScriptRoot" --tag "$tag"
-    docker run --detach --interactive --name "$tag" "$tag"
+BeforeAll{
+    $name = Split-Path "$PSScriptRoot" -Leaf
+
+    $ancestor = Split-Path "$PSScriptRoot" -Parent
+    while ($true) {
+        if (Get-ChildItem $ancestor "License.txt") {
+            if ($Env:GITHUB_REPOSITORY) {
+                $name = "$("$Env:GITHUB_REPOSITORY".Split("/")[0])/$name"
+            }
+            else {
+                $ancestor = Split-Path $ancestor -Parent
+                $name = "$(Split-Path $ancestor -Leaf)/$name"
+            }
+
+            break
+        }
+
+        $name = "$(Split-Path $ancestor -Leaf)/$name"
+        $ancestor = Split-Path $ancestor -Parent
+    }
+
+    $tag = "$name`:test"
+    Write-Warning $tag
+
+    docker build "$PSScriptRoot" --tag "$tag" 2>&1 > $null
+    $containerId = docker run --detach --interactive "$tag"
 }
 
 AfterAll {
-    docker stop "$tag"
-    docker rm "$tag"
+    docker stop "$containerId"
+    docker rm "$containerId"
     docker image rm --force "$tag"
 }
 
 Describe "ci/powershell" {
     It "can be used to exec a command" {
         # Act
-        $version = docker exec "$tag" dotnet --list-runtimes
+        $version = docker exec "$containerId" dotnet --list-runtimes
 
         # Assert
         $version | Should -BeExactly "Microsoft.NETCore.App 8.0.3 [/usr/share/dotnet/shared/Microsoft.NETCore.App]"
--- hugo/Dockerfile.Tests.ps1
+++ hugo/Dockerfile.Tests.ps1
@@ -1,5 +1,27 @@
 BeforeAll {
-    $tag = [System.Guid]::NewGuid().ToString("N")
+    $name = Split-Path "$PSScriptRoot" -Leaf
+
+    $ancestor = Split-Path "$PSScriptRoot" -Parent
+    while ($true) {
+        if (Get-ChildItem $ancestor "License.txt") {
+            if ($Env:GITHUB_REPOSITORY) {
+                $name = "$("$Env:GITHUB_REPOSITORY".Split("/")[0])/$name"
+            }
+            else {
+                $ancestor = Split-Path $ancestor -Parent
+                $name = "$(Split-Path $ancestor -Leaf)/$name"
+            }
+
+            break
+        }
+
+        $name = "$(Split-Path $ancestor -Leaf)/$name"
+        $ancestor = Split-Path $ancestor -Parent
+    }
+
+    $tag = "$name`:test"
+    Write-Warning $tag
+
     docker build "$PSScriptRoot" --tag "$tag" 2>&1 > $null
 }
 
