ad67367a966aa9069874aaeaf32d4b8588526ec9
Michiel van Oosterhout <git@michielvoo.net>
2024-03-17 07:51:38 +0300

    Use fully qualified image names when building for tests

--- ci/powershell/Dockerfile.Tests.ps1
+++ ci/powershell/Dockerfile.Tests.ps1
@@ -1,19 +1,34 @@
-BeforeAll {
-    $tag = [System.Guid]::NewGuid().ToString("N")
-    docker build "$PSScriptRoot" --tag "$tag"
-    docker run --detach --interactive --name "$tag" "$tag"
+BeforeAll{
+    $name = Split-Path "$PSScriptRoot" -Leaf
+
+    $ancestor = Split-Path "$PSScriptRoot" -Parent
+    while ($true) {
+        if (Get-ChildItem $ancestor "License.txt") {
+            $ancestor = Split-Path $ancestor -Parent
+            $name = "$(Split-Path $ancestor -Leaf)/$name"
+            break
+        }
+
+        $name = "$(Split-Path $ancestor -Leaf)/$name"
+        $ancestor = Split-Path $ancestor -Parent
+    }
+
+    $tag = "$name`:test"
+
+    docker build "$PSScriptRoot" --tag "$tag" 2>&1 > $null
+    $containerId = docker run --detach --interactive "$tag"
 }
 
 AfterAll {
-    docker stop "$tag"
-    docker rm "$tag"
+    docker stop "$containerId"
+    docker rm "$containerId"
     docker image rm --force "$tag"
 }
 
 Describe "ci/powershell" {
     It "can be used to exec a command" {
         # Act
-        $version = docker exec "$tag" dotnet --list-runtimes
+        $version = docker exec "$containerId" dotnet --list-runtimes
 
         # Assert
         $version | Should -BeExactly "Microsoft.NETCore.App 8.0.3 [/usr/share/dotnet/shared/Microsoft.NETCore.App]"
--- hugo/Dockerfile.Tests.ps1
+++ hugo/Dockerfile.Tests.ps1
@@ -1,5 +1,20 @@
 BeforeAll {
-    $tag = [System.Guid]::NewGuid().ToString("N")
+    $name = Split-Path "$PSScriptRoot" -Leaf
+
+    $ancestor = Split-Path "$PSScriptRoot" -Parent
+    while ($true) {
+        if (Get-ChildItem $ancestor "License.txt") {
+            $ancestor = Split-Path $ancestor -Parent
+            $name = "$(Split-Path $ancestor -Leaf)/$name"
+            break
+        }
+
+        $name = "$(Split-Path $ancestor -Leaf)/$name"
+        $ancestor = Split-Path $ancestor -Parent
+    }
+
+    $tag = "$name`:test"
+
     docker build "$PSScriptRoot" --tag "$tag" 2>&1 > $null
 }
 
